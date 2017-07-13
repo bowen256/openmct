ffbe52402214ec1cd254188295ccbe6018836a31
Pete Richards <peter.l.richards@nasa.gov>
2017-07-12 18:40:53 -0700

    some work on latest value table

--- src/plugins/telemetryTable/plugin.js
+++ src/plugins/telemetryTable/plugin.js
@@ -1,27 +1,67 @@
 import etch from 'etch';
 
-class MyListView {
-    constructor (domainObject) {
-        this.domainObject = domainObject;
-        etch.initialize(this);
-    }
-    render () {
-        return etch.dom('div', {}, `Viewing: ${this.domainObject.name}`);
-    }
-    show (container) {
-        etch.update(this);
-        container.appendChild(this.element)
-    }
-    destroy(container) {
-        container.removeChild(this.element);
-        etch.destroy(this);
-    }
-}
+const d = etch.dom;
 
 module.exports = function TelemetryTablePlugin() {
     return function install(openmct) {
-        console.log('installed!');
-        console.log(etch);
+
+        class ListItemView {
+            constructor (props, children) {
+                this.props = props;
+                this.destroy = openmct.objects.observe(this.props.domainObject, '*', this.onMutation.bind(this));
+                etch.initialize(this);
+            }
+            onMutation (domainObject) {
+                this.props.domainObject = domainObject;
+                etch.update(this);
+            }
+            render () {
+                return d('li', null, `${this.props.domainObject.name}`);
+            }
+            update (props, children) {
+                return etch.update(this)
+            }
+        }
+
+        class ListView {
+            constructor (properties, children) {
+                this.props = properties;
+                this.props.children = this.props.children || [];
+                this.composition = openmct.composition.get(this.props.domainObject);
+                this.composition.on('add', this.addChild, this);
+                this.composition.on('remove', this.removeChild, this);
+                etch.initialize(this);
+                this.composition.load();
+            }
+            render () {
+                return etch.dom('div', {},
+                    d('h1', null, `Viewing: ${this.props.domainObject.name}`),
+                    d('ul', null,
+                        this.props.children.map((c) => d(ListItemView, {domainObject: c}))
+                    )
+                );
+            }
+            destroy () {
+                this.composition.off('add', this.addChild, this);
+                this.composition.off('remove', this.removeChild, this);
+                delete this.composition;
+            }
+            addChild (child) {
+                this.props.children.push(child);
+                etch.update(this);
+            }
+            removeChild (cid) {
+                this.props.children = this.props.children.filter(function (c) {
+                    return !(c.identifier.key === cid.key && c.identifier.namespace === cid.namespace)
+                });
+                etch.update(this);
+            }
+            update (props, children) {
+                this.domainObject = properties.domainObject;
+                this.children = properties.children || [];
+                return etch.update(this)
+            }
+        }
 
         openmct.objectViews.addProvider({
             name: 'my view',
@@ -29,7 +69,17 @@ module.exports = function TelemetryTablePlugin() {
                 return d.type === 'folder' && 150;
             },
             view: function (domainObject) {
-                return new MyListView(domainObject);
+
+                var listView = new ListView({domainObject: domainObject});
+
+                return {
+                    show: function (container) {
+                        container.appendChild(listView.element);
+                    },
+                    destroy: function (container) {
+                        etch.destroy(listView);
+                    }
+                };
             }
         });
     };
