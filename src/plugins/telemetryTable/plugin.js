5418ea9d0a508efd1f5b01bbda813077294f6110
Pete Richards <peter.l.richards@nasa.gov>
2017-07-12 17:41:40 -0700

    basic view

--- src/plugins/telemetryTable/plugin.js
+++ src/plugins/telemetryTable/plugin.js
@@ -1,9 +1,37 @@
 import etch from 'etch';
 
+class MyListView {
+    constructor (domainObject) {
+        this.domainObject = domainObject;
+        etch.initialize(this);
+    }
+    render () {
+        return etch.dom('div', {}, `Viewing: ${this.domainObject.name}`);
+    }
+    show (container) {
+        etch.update(this);
+        container.appendChild(this.element)
+    }
+    destroy(container) {
+        container.removeChild(this.element);
+        etch.destroy(this);
+    }
+}
+
 module.exports = function TelemetryTablePlugin() {
     return function install(openmct) {
         console.log('installed!');
         console.log(etch);
+
+        openmct.objectViews.addProvider({
+            name: 'my view',
+            canView: function (d) {
+                return d.type === 'folder' && 150;
+            },
+            view: function (domainObject) {
+                return new MyListView(domainObject);
+            }
+        });
     };
 };
 
